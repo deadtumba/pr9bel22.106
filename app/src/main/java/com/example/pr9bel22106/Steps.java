2baf77ad5309d69021ebe0326e7f88bb3ffb8b01
kirill <gekagekov1@gmail.com>
2023-06-30 22:17:58 +0700

    ff

---
+++ app/src/main/java/com/example/pr9bel22106/Steps.java
@@ -0,0 +1,14 @@
+package com.example.pr9bel22106;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Steps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_steps);
+    }
+}
